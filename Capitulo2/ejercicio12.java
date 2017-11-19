8329b7fed0bcf7c9a63ef00eb42e22cb5b82b933
mijail73 <mijail.iso@ciencias.unam.mx>
2017-11-19 16:23:35 -0600

    Capitulo 2

---
+++ Capitulo2/ejercicio12.java
@@ -0,0 +1,9 @@
+public class ejercicio12 {
+    public static void main(String[] args) {
+        final int FACTOR = 10;
+        int a = 1;
+        int b = 2;
+        int complejo = a + b * 3 - a * b + 3;
+        System.out.println(complejo);
+    }
+}
---
+++ Capitulo2/ejercicio13.java
@@ -0,0 +1,11 @@
+public class ejercicio13 {
+    public static void main(String[] args) {
+        final int FACTOR = 10;
+        int a = 4;
+        int b = 6;
+        int c = (a + b) * FACTOR;
+        int d = a + b * FACTOR;
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
